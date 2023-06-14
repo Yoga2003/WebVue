65f6d91ed0e2d9e86899f9f3ce7f496b1d92e285
Yoga2003 <yogadwisusena@students.amikom.ac.id>
2023-06-14 23:22:40 +0700

    Tugas Vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+    <NavBar />
+  </header>
+
+  <HeroComponent />
+  <ProdukSection />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from "./components/NavBar.vue";
+import HeroComponent from "./components/HeroComponent.vue";
+import ProdukSection from "./components/ProdukSection.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    NavBar,
+    HeroComponent,
+    ProdukSection
+},
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
